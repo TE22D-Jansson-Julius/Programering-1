0484be5b2fd9dac642fa3c8472ff371e0d13a337
Bullen <julius.jansson@elev.ga.ntig.se>
2023-08-31 10:39:19 +0200

    egwy8

---
+++ Hejsan/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+string name = "MickeB";
+
+Console.WriteLine("Hej " + name + " vilken sexig banan du är idag!<3");
+
